12dd851185e56249fa12c877c4ea41b1e6a0567c
cursedninja <sairam413@gmail.com>
2014-09-06 04:40:59 -0400

    Adding Constants

---
+++ app/src/main/java/com/alternativeheroes/mhacks/dropped/Constants.java
@@ -0,0 +1,13 @@
+package com.alternativeheroes.mhacks.dropped;
+
+/**
+ * Created by NiNJa on 9/6/2014.
+ */
+public class Constants {
+    public static String dropEventAction = "com.alternateheroes.mhacks.dropped.dropEventAction";
+    public static String dropPreference = "com.alternateheroes.mhacks.dropped.dropPreference";
+
+    public static int DROPTYPE_MUSIC = 0;
+    public static int DROPTYPE_SCREAM = 1;
+    public static String dropType = "com.alternateheroes.mhacks.dropped.dropType";
+}
